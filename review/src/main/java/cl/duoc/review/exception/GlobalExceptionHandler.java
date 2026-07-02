656163b20d5c58e9b5e5957e24c4fc853c94f7b4
Antonio <an.cayulen@duocuc.cl>
2026-07-02 12:11:45 -0400

    Adicion de logs de error

--- review/src/main/java/cl/duoc/review/exception/GlobalExceptionHandler.java
+++ review/src/main/java/cl/duoc/review/exception/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package cl.duoc.review.exception;
 
 import cl.duoc.review.dto.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -9,7 +11,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.stream.Collectors;
-
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -18,7 +20,7 @@ public class GlobalExceptionHandler {
         String errorMessage = ex.getBindingResult().getFieldErrors().stream()
                 .map(FieldError::getDefaultMessage)
                 .collect(Collectors.joining(", "));
-        
+        log.error("Error al crear reseña");
         ApiResponse<Object> response = new ApiResponse<>(HttpStatus.BAD_REQUEST.value(), errorMessage, null);
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
@@ -26,6 +28,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiResponse<Object>> handleGeneralExceptions(Exception ex) {
         ApiResponse<Object> response = new ApiResponse<>(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Error interno: " + ex.getMessage(), null);
+        log.error("Error interno al crear reseña");
         return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
